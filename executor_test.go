eacb9e27ebc62675fd8f3807d1bd871fb711d4a3
Jesko Plitt <jp@becklyn.com>
2024-08-14 12:52:27 +0200

    Fix executor tests

--- executor_test.go
+++ executor_test.go
@@ -754,7 +754,7 @@ func TestThrowsIfNoOperationIsProvided(t *testing.T) {
 
 	expectedErrors := []gqlerrors.FormattedError{
 		{
-			Message:   "Must provide an operation.",
+			Message:   "must provide an operation",
 			Locations: []location.SourceLocation{},
 		},
 	}
@@ -799,7 +799,7 @@ func TestThrowsIfNoOperationNameIsProvidedWithMultipleOperations(t *testing.T) {
 
 	expectedErrors := []gqlerrors.FormattedError{
 		{
-			Message:   "Must provide operation name if query contains multiple operations.",
+			Message:   "must provide operation name if query contains multiple operations",
 			Locations: []location.SourceLocation{},
 		},
 	}
@@ -844,7 +844,7 @@ func TestThrowsIfUnknownOperationNameIsProvided(t *testing.T) {
 
 	expectedErrors := []gqlerrors.FormattedError{
 		{
-			Message:   `Unknown operation named "UnknownExample".`,
+			Message:   `unknown operation named "UnknownExample"`,
 			Locations: []location.SourceLocation{},
 		},
 	}
